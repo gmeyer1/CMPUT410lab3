2096aa40fbacd57cf173f7ebbb3047be6328e1cf
gmeyer1 <gmeyer1@ualberta.ca>
2015-01-28 12:53:21 -0700

    initial commit - added server.py

---
+++ server/test_cgi.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+ 
+print "Content-type: text/html"
+print
+print "<title>Name+Family</title>"
+print "<form>"
+print "Name:<br>"
+print "<input type=\"text\" name=\"name\">"
+print "<br>"
+print "Family:<br>"
+print "<input type=\"text\" name=\"family\">"
+print "</form>"
---
+++ server/test_cgi_2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+ 
+print "Content-type: text/html"
+print
+print "<title>Birthdate+Hobby</title>"
+print "<form>"
+print "Birthdate:<br>"
+print "<input type=\"text\" name=\"birthdate\">"
+print "<br>"
+print "Main Hobby:<br>"
+print "<input type=\"text\" name=\"hobby\">"
+print "</form>"
