889d9d49fa66d7344f920eda20d47db7f830e2ef
gmeyer1 <gmeyer1@ualberta.ca>
2015-01-28 15:33:45 -0700

    cgi files tested and working

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # CMPUT410lab3
+
+http://localhost:8000/cgi/form_1.py
+http://localhost:8000/cgi/form_2.py
--- server/cgi/form_1.py
+++ server/cgi/form_1.py
@@ -1,17 +1,8 @@
 #!/usr/bin/env python
 
-import cgi
-form = cgi.FieldStorage()
-birthdate=form.getvalue('birthdate')
-hobby=form.getvalue('hobby')
-print "Birthdate: <br/>"
-print birthdate
-print "Main Hobby: <br/>"
-print hobby
-
 print """Content-type: text/html
 
-<title>Name+Family</title>
+<title>Name & Family</title>
 <form method="post" action="form_2.py">
 Name:<br>
 <input type="text" name="name"><br>
@@ -20,3 +11,14 @@ Family:<br>
 <input type="submit" value="Submit"><br>
 </form>
 """
+
+import cgi
+form = cgi.FieldStorage()
+birthdate=form.getvalue('birthdate')
+hobby=form.getvalue('hobby')
+print "Birthdate: "
+print birthdate
+print "<br/>"
+print "Main Hobby: "
+print hobby
+print "<br/>"
